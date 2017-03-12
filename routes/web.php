b3b426735c20fea50eec866c7f5f849de54a06a1
fiftysoft <fiftysoft@gmail.com>
2017-03-12 16:00:02 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,15 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Auth Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| this is route for auth users
 |
 */
 Route::group(['prefix'=>'/interface'],function ()
 {
-Route::get('/login','Auth\LoginController@showLoginForm')->name('login')->middleware('notAllowed');
-Route::post('/login','Auth\LoginController@login');
-Route::post('/logout','Auth\LoginController@logout');
+  Route::get('/login','Auth\LoginController@showLoginForm')->name('login')->middleware('notAllowed');
+  Route::post('/login','Auth\LoginController@login');
+  Route::post('/logout','Auth\LoginController@logout');
 });
 
