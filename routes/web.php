565cead14bb84e42a401669553e9b69bf6fcfb09
fiftysoft <fiftysoft@gmail.com>
2017-03-12 16:01:00 +0300

    Update web.php
    remove middleware from login route

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@
 */
 Route::group(['prefix'=>'/interface'],function ()
 {
-  Route::get('/login','Auth\LoginController@showLoginForm')->name('login')->middleware('notAllowed');
+  Route::get('/login','Auth\LoginController@showLoginForm');
   Route::post('/login','Auth\LoginController@login');
   Route::post('/logout','Auth\LoginController@logout');
 });
