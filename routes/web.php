f48d39a4cd17d24f5c20ca02b8aad43cfa5350e3
Muhammed <mohammetguzil@gmail.com>
2017-03-13 14:58:20 +0300

    Added Missing files

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,11 @@
 | this is route for auth users
 |
 */
-Route::group(['prefix'=>'/interface'],function ()
+Route::group(['prefix'=>'/interface','middleware'=>'notAllowed'],function ()
 {
-  Route::get('/login','Auth\LoginController@showLoginForm');
+  Route::get('/login','Admin\LoginController@showLoginForm');
   Route::post('/login','Auth\LoginController@login');
   Route::post('/logout','Auth\LoginController@logout');
+  Route::get('deny','Auth\LoginController@deny');
 });
 
