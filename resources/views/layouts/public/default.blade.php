e792b05469f75113d2a9b22c3b9c8a71dd1ec15c
Muhammed <mohammetguzil@gmail.com>
2017-03-14 12:50:35 +0300

    Fixed minor issues

--- resources/views/layouts/public/default.blade.php
+++ resources/views/layouts/public/default.blade.php
@@ -6,17 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{url('Public/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{url('Public/css/font-awesome.min.css')}}">
-    <link rel="stylesheet" href="{{url('Public/css/icofont.css')}}">
-    <link rel="stylesheet" href="{{url('Public/css/slidr.css')}}">
-    <link rel="stylesheet" href="{{url('Public/css/main.css')}}">
-    <link id="preset" rel="stylesheet" href="{{url('Public/css/presets/preset1.css')}}">
-    <link rel="stylesheet" href="{{url('Public/css/responsive.css')}}">
+    <link rel="stylesheet" href="{{url('public/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{url('public/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{url('public/css/icofont.css')}}">
+    <link rel="stylesheet" href="{{url('public/css/slidr.css')}}">
+    <link rel="stylesheet" href="{{url('public/css/main.css')}}">
+    <link id="preset" rel="stylesheet" href="{{url('public/css/presets/preset1.css')}}">
+    <link rel="stylesheet" href="{{url('public/css/responsive.css')}}">
     <link href='https://fonts.googleapis.com/css?family=Ubuntu:400,500,700,300' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Signika+Negative:400,300,600,700' rel='stylesheet'
           type='text/css'>
-    <link rel="icon" href="{{url('Public/images/ico/favicon.ico')}}">
+    <link rel="icon" href="{{url('public/images/ico/favicon.ico')}}">
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -29,11 +29,11 @@
 @yield('content')
 
 @include('layouts.public.footer')
-<script src="{{url('Public/js/jquery.min.js')}}"></script>
-<script src="{{url('Public/js/bootstrap.min.js')}}"></script>
-<script src="{{url('Public/js/price-range.js')}}"></script>
-<script src="{{url('Public/js/main.js')}}"></script>
-<script src="{{url('Public/js/switcher.js')}}"></script>
+<script src="{{url('public/js/jquery.min.js')}}"></script>
+<script src="{{url('public/js/bootstrap.min.js')}}"></script>
+<script src="{{url('public/js/price-range.js')}}"></script>
+<script src="{{url('public/js/main.js')}}"></script>
+<script src="{{url('public/js/switcher.js')}}"></script>
 @yield('js')
 </body>
 </html>
