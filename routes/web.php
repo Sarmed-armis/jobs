318e6cbe74446f2b6b8bf20c4176fb58556e8b12
Your Name <sarmed_mahmood@yahoo.com>
2017-03-23 13:35:47 +0300

    user controller

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,8 @@ Route::group(['prefix'=>'/interface','middleware'=>'AllowedIP'],function ()
   Route::get('/login','Admin\Auth\LoginController@showLoginForm');
   Route::post('/login','Admin\Auth\LoginController@login');
   Route::post('/logout','Admin\Auth\LoginController@logout');
+    Route::get('/user','Admin\User@index');
+
 });
 Route::get('deny','Admin\Auth\LoginController@deny');
+
